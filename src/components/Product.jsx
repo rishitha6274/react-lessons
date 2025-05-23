629b801e0f8dcfba6754b27da8c7d2d31d513a7e
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-23 21:15:06 +0530

    updated

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,7 @@
-export default function Product(props) {
-  return  <div className="App-Product-Box">
-      <h3>{props.value.name}</h3>
-      <h4>${props.value.price}</h4>
-      <p><button>Add to Cart</button></p>
-    </div>;
+export default function Product(value) {
+  return <div className="App-Product-Box">
+    <h2>{value.name}</h2>
+    <h3>{value.price}</h3>
+    <button>Add to Cart</button>
+  </div>;
 }
