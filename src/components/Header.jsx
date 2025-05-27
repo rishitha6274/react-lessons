ac0f2ac6d2251c24745a2ba41fe33dd6521e1794
rishitha6274 <rishithalattupally01@gmail.com>
2025-05-27 20:20:12 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ export default function Header() {
       <Link to="/app13">App13</Link> - <Link to="/app14">App14</Link> -
       <Link to="/app15">App15</Link> - <Link to="/app16">App16</Link> -
       <Link to="/app17">App17</Link> - <Link to="/app18">App18</Link> -
-      <Link to="/app19">App19</Link>
+      <Link to="/app19">App19</Link> - <Link to="/app20">App20</Link>
     </div>
   );
 }
